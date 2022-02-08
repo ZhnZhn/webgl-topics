197f87414a7e25a7035499f35a8cb4331c5bd01b
Yevhenii Molodyi <eugmolody@gmail.com>
2022-02-08 14:36:25 +0200

    Add crCn to ButtonCircle2

--- js/components/zhn-atoms/ButtonCircle2.js
+++ js/components/zhn-atoms/ButtonCircle2.js
@@ -9,23 +9,24 @@ var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends")
 
 var _objectWithoutPropertiesLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutPropertiesLoose"));
 
+var _crCn = _interopRequireDefault(require("../zhn-utils/crCn"));
+
 var _jsxRuntime = require("react/jsx-runtime");
 
-var CL = 'zhn-bt-circle2';
+var _excluded = ["className", "style", "caption", "onClick"];
+var CL_BT_CIRCLE_2 = 'zhn-bt-circle2';
 
 var ButtonCircle2 = function ButtonCircle2(_ref) {
-  var _ref$className = _ref.className,
-      className = _ref$className === void 0 ? '' : _ref$className,
+  var className = _ref.className,
       style = _ref.style,
       _ref$caption = _ref.caption,
       caption = _ref$caption === void 0 ? '' : _ref$caption,
       onClick = _ref.onClick,
-      rest = (0, _objectWithoutPropertiesLoose2["default"])(_ref, ["className", "style", "caption", "onClick"]);
-  return /*#__PURE__*/(0, _jsxRuntime.jsx)("button", (0, _extends2["default"])({
-    className: CL + " " + className,
+      rest = (0, _objectWithoutPropertiesLoose2["default"])(_ref, _excluded);
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("button", (0, _extends2["default"])({}, rest, {
+    className: (0, _crCn["default"])(CL_BT_CIRCLE_2, className),
     style: style,
-    onClick: onClick
-  }, rest, {
+    onClick: onClick,
     children: /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
       children: caption
     })
--- src/components/zhn-atoms/ButtonCircle2.jsx
+++ src/components/zhn-atoms/ButtonCircle2.jsx
@@ -1,17 +1,19 @@
-const CL = 'zhn-bt-circle2';
+import crCn from '../zhn-utils/crCn';
+
+const CL_BT_CIRCLE_2 = 'zhn-bt-circle2';
 
 const ButtonCircle2 = ({
-  className='',
+  className,
   style,
   caption='',
   onClick,
   ...rest
 }) => (
   <button
-     className={`${CL} ${className}`}
+     {...rest}
+     className={crCn(CL_BT_CIRCLE_2, className)}
      style={style}
      onClick={onClick}
-     {...rest}
   >
     <div>
       {caption}
