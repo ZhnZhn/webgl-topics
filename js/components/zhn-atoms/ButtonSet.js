39125bdfc3204946fc55fcef8b57d812e36f68aa
Yevhenii Molodyi <eugmolody@gmail.com>
2022-02-08 15:06:11 +0200

    Replace ButtonSet by function component

--- js/components/zhn-atoms/ButtonSet.js
+++ js/components/zhn-atoms/ButtonSet.js
@@ -5,73 +5,75 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
 var _react = require("react");
 
-var _ButtonSet = _interopRequireDefault(require("./ButtonSet.Style"));
-
 var _jsxRuntime = require("react/jsx-runtime");
 
-var _hmModeStyle = {
-  0: (0, _extends2["default"])({}, _ButtonSet["default"].BT, _ButtonSet["default"].NOT_VALID),
-  1: (0, _extends2["default"])({}, _ButtonSet["default"].BT, _ButtonSet["default"].VALID_CHANGED),
-  2: (0, _extends2["default"])({}, _ButtonSet["default"].BT, _ButtonSet["default"].VALID_NOT_CHANGED)
+var CL_BT = "bt",
+    S_BT = {
+  color: '#fefcff',
+  height: 30,
+  padding: '4px 6px',
+  marginLeft: 16,
+  fontSize: '16px',
+  fontWeight: 'bold',
+  backgroundColor: '#673ab7',
+  border: '2px solid #673ab7',
+  borderRadius: 8
+},
+    S_VALID_CHANGED = {
+  border: '2px solid #673ab7'
+},
+    S_VALID_NOT_CHANGED = {
+  backgroundColor: '#9e9e9e',
+  border: '2px solid #9e9e9e'
+},
+    S_NOT_VALID = {
+  color: '#f44336',
+  backgroundColor: '#9e9e9e',
+  border: '2px solid #9e9e9e'
 };
-
-var ButtonSet = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(ButtonSet, _Component);
-
-  /*
-  static propTypes = {
-    mode: PropTypes.number,
-    onClick: PropTypes.func
-  }
-  */
-  function ButtonSet(props) {
-    var _this;
-
-    _this = _Component.call(this, props) || this;
-
-    _this.setMode = function (mode) {
-      _this.setState({
-        mode: mode
-      });
-    };
-
-    _this.state = {
-      mode: props.mode
-    };
-    return _this;
-  }
-
-  var _proto = ButtonSet.prototype;
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        style = _this$props.style,
-        onClick = _this$props.onClick,
-        mode = this.state.mode,
-        _style = _hmModeStyle[mode],
-        _onClick = mode === 1 ? onClick : void 0;
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)("button", {
-      className: "bt",
-      style: (0, _extends2["default"])({}, style, _style),
-      onClick: _onClick,
-      children: "Set"
-    });
-  };
-
-  return ButtonSet;
-}(_react.Component);
-
-ButtonSet.defaultProps = {
-  mode: 2,
-  onClick: function onClick() {}
+var HM_MODE_STYLE = {
+  0: (0, _extends2["default"])({}, S_BT, S_NOT_VALID),
+  1: (0, _extends2["default"])({}, S_BT, S_VALID_CHANGED),
+  2: (0, _extends2["default"])({}, S_BT, S_VALID_NOT_CHANGED)
 };
+var ButtonSet = /*#__PURE__*/(0, _react.forwardRef)(function (_ref, ref) {
+  var _ref$mode = _ref.mode,
+      mode = _ref$mode === void 0 ? 2 : _ref$mode,
+      style = _ref.style,
+      onClick = _ref.onClick;
+
+  var _useState = (0, _react.useState)(mode),
+      btMode = _useState[0],
+      setBtMode = _useState[1];
+
+  (0, _react.useImperativeHandle)(ref, function () {
+    return {
+      setMode: setBtMode
+    };
+  }, []);
+
+  var _style = HM_MODE_STYLE[btMode],
+      _onClick = btMode === 1 ? onClick : void 0;
+
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("button", {
+    className: CL_BT,
+    style: (0, _extends2["default"])({}, style, _style),
+    onClick: _onClick,
+    children: "Set"
+  });
+});
+/*
+ButtonSet.propTypes = {
+  mode: PropTypes.number,
+  style: PropTypes.object,
+  onClick: PropTypes.func
+}
+*/
+
 var _default = ButtonSet;
 exports["default"] = _default;
 //# sourceMappingURL=ButtonSet.js.map
--- src/components/zhn-atoms/ButtonSet.jsx
+++ src/components/zhn-atoms/ButtonSet.jsx
@@ -1,53 +1,73 @@
-import { Component } from 'react';
+import { 
+  forwardRef,
+  useState,
+  useImperativeHandle
+} from 'react';
 
-import STYLE from './ButtonSet.Style';
-
-const _hmModeStyle = {
-  0 : { ...STYLE.BT, ...STYLE.NOT_VALID },
-  1 : { ...STYLE.BT, ...STYLE.VALID_CHANGED },
-  2 : { ...STYLE.BT, ...STYLE.VALID_NOT_CHANGED },
+const CL_BT = "bt"
+, S_BT = {
+  color: '#fefcff',
+  height: 30,
+  padding: '4px 6px',
+  marginLeft: 16,
+  fontSize: '16px',
+  fontWeight: 'bold',
+  backgroundColor: '#673ab7',
+  border: '2px solid #673ab7',
+  borderRadius: 8
 }
-
-class ButtonSet extends Component{
-  /*
-  static propTypes = {
-    mode: PropTypes.number,
-    onClick: PropTypes.func
-  }
-  */
-  static defaultProps = {
-    mode: 2,
-    onClick: () => {}
-  }
-
-  constructor(props){
-    super(props);
-    this.state = {
-      mode : props.mode
-    }
-  }
-
-  render(){
-    const { style, onClick } = this.props
-        , { mode } = this.state
-        , _style = _hmModeStyle[mode]
-        , _onClick = (mode === 1)
-             ? onClick
-             : void 0;
-    return (
-      <button
-        className="bt"
-        style={{ ...style, ..._style}}
-        onClick={_onClick}
-      >
-       Set
-      </button>
-    );
-  }
-
-  setMode = (mode) => {
-    this.setState({ mode })
-  }
+, S_VALID_CHANGED = {
+  border: '2px solid #673ab7'
 }
+, S_VALID_NOT_CHANGED = {
+  backgroundColor: '#9e9e9e',
+  border: '2px solid #9e9e9e'
+}
+, S_NOT_VALID = {
+  color: '#f44336',
+  backgroundColor: '#9e9e9e',
+  border: '2px solid #9e9e9e'
+};
+
+const HM_MODE_STYLE = {
+  0 : {...S_BT, ...S_NOT_VALID},
+  1 : {...S_BT, ...S_VALID_CHANGED},
+  2 : {...S_BT, ...S_VALID_NOT_CHANGED},
+};
+
+const ButtonSet = forwardRef(({
+  mode=2,
+  style,
+  onClick
+}, ref) => {
+  const [btMode, setBtMode] = useState(mode);
+
+  useImperativeHandle(ref, () => ({
+    setMode: setBtMode
+  }), [])
+
+  const _style = HM_MODE_STYLE[btMode]
+  , _onClick = (btMode === 1)
+      ? onClick
+      : void 0;
+
+  return (
+    <button
+      className={CL_BT}
+      style={{...style, ..._style}}
+      onClick={_onClick}
+    >
+     Set
+    </button>
+  );
+});
+
+/*
+ButtonSet.propTypes = {
+  mode: PropTypes.number,
+  style: PropTypes.object,
+  onClick: PropTypes.func
+}
+*/
 
 export default ButtonSet
