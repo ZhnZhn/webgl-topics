7307b9b8fc0ac02d1590a928e666dae4df8fe590
Yevhenii Molodyi <eugmolody@gmail.com>
2022-07-11 15:07:25 +0300

    Add useProperty

--- js/components/uiApi.js
+++ js/components/uiApi.js
@@ -1,12 +1,14 @@
 "use strict";
 
 exports.__esModule = true;
-exports.useState = exports.useImperativeHandle = exports.forwardRef = exports.Component = void 0;
+exports.useState = exports.useRef = exports.useMemo = exports.useImperativeHandle = exports.forwardRef = exports.Component = void 0;
 
 var _react = require("react");
 
 exports.Component = _react.Component;
 exports.forwardRef = _react.forwardRef;
+exports.useRef = _react.useRef;
 exports.useState = _react.useState;
+exports.useMemo = _react.useMemo;
 exports.useImperativeHandle = _react.useImperativeHandle;
 //# sourceMappingURL=uiApi.js.map
--- src/components/uiApi.js
+++ src/components/uiApi.js
@@ -1,6 +1,8 @@
 export {
   Component,
   forwardRef,
+  useRef,
   useState,
+  useMemo,
   useImperativeHandle
 } from 'react';
