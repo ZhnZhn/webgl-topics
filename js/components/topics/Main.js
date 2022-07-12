27d5fa3a05f631808dca59ac5cd4caf6fff08e58
Yevhenii Molodyi <eugmolody@gmail.com>
2022-07-12 10:54:40 +0300

    Edit Main css style

--- js/components/topics/Main.js
+++ js/components/topics/Main.js
@@ -7,7 +7,7 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _react = require("react");
+var _uiApi = require("../uiApi");
 
 var _useListen = _interopRequireDefault(require("../hooks/useListen"));
 
@@ -21,14 +21,17 @@ var _dfValues = _interopRequireDefault(require("./gl-props/dfValues"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
+var CL_CONTAINER = 'container',
+    CL_CONTENT = CL_CONTAINER + "__content";
+
 var TopicWrapper = function TopicWrapper(_ref) {
   var store = _ref.store;
 
-  var _useState = (0, _react.useState)(),
+  var _useState = (0, _uiApi.useState)(),
       topicId = _useState[0],
       setTopicId = _useState[1],
-      refComp = (0, _react.useRef)(null),
-      getComponentTopic = (0, _react.useCallback)(function () {
+      refComp = (0, _uiApi.useRef)(null),
+      getComponentTopic = (0, _uiApi.useCallback)(function () {
     return refComp.current;
   }, []);
 
@@ -41,15 +44,14 @@ var TopicWrapper = function TopicWrapper(_ref) {
   var _factoryTopic = (0, _factoryTopic2["default"])(topicId),
       Comp = _factoryTopic.Comp,
       compProps = _factoryTopic.props,
-      _compProps$valuesForI = compProps.valuesForInit,
-      valuesForInit = _compProps$valuesForI === void 0 ? {} : _compProps$valuesForI,
+      valuesForInit = compProps.valuesForInit,
       _valuesForInit = (0, _extends2["default"])({}, _dfValues["default"], valuesForInit);
 
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-    className: "container",
+    className: CL_CONTAINER,
     role: "document",
     children: /*#__PURE__*/(0, _jsxRuntime.jsx)("main", {
-      className: "container__content",
+      className: CL_CONTENT,
       role: "main",
       children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
         className: "row",
--- src/components/topics/Main.jsx
+++ src/components/topics/Main.jsx
@@ -1,17 +1,28 @@
-import { useState, useRef, useCallback } from 'react';
+import {
+  useState,
+  useRef,
+  useCallback
+} from '../uiApi';
 
-import useListen from '../hooks/useListen'
+import useListen from '../hooks/useListen';
 
-import { RouterTopicActionTypes } from '../../flux/actions/RouterTopicActions'
+import { RouterTopicActionTypes } from '../../flux/actions/RouterTopicActions';
 
 import factoryTopic from './factoryTopic';
 import PanelConfigGL from '../panel-config-gl/PanelConfigGL';
 
 import dfValues from './gl-props/dfValues';
 
+const CL_CONTAINER = 'container'
+, CL_CONTENT = `${CL_CONTAINER}__content`;
 
-const TopicWrapper = ({ store }) => {
-  const [topicId, setTopicId] = useState()
+const TopicWrapper = ({
+  store
+}) => {
+  const [
+    topicId,
+    setTopicId
+  ] = useState()
   , refComp = useRef(null)
   , getComponentTopic = useCallback(() => refComp.current, []);
 
@@ -21,13 +32,19 @@ const TopicWrapper = ({ store }) => {
     }
   })
 
-  const { Comp, props:compProps } = factoryTopic(topicId)
-  , { valuesForInit={} } = compProps
-  , _valuesForInit = { ...dfValues, ...valuesForInit };
+  const {
+    Comp,
+    props:compProps
+  } = factoryTopic(topicId)
+  , { valuesForInit } = compProps
+  , _valuesForInit = {
+      ...dfValues,
+      ...valuesForInit
+  };
 
   return (
-    <div className="container" role="document">
-      <main className="container__content" role="main">
+    <div className={CL_CONTAINER} role="document">
+      <main className={CL_CONTENT} role="main">
         <div className="row">
           <Comp
              ref={refComp}
