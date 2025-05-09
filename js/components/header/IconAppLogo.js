f15a1009965608ce28204d6095e3bfe040fb9e5f
Yevhenii Molodyi <eugmolody@gmail.com>
2025-05-09 13:39:49 +0300

    Improve IconAppLogo function component

--- js/components/header/IconAppLogo.js
+++ js/components/header/IconAppLogo.js
@@ -1,61 +1,38 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
-exports["default"] = void 0;
-
-var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
-
+exports.default = void 0;
 var _SvgStyle = require("./SvgStyle");
-
 var _jsxRuntime = require("preact/jsx-runtime");
-
-var IconAppLogo = function IconAppLogo(_ref) {
-  var className = _ref.className,
-      title = _ref.title;
+const _crRect = (rx, y, x, height, width, color) => (0, _jsxRuntime.jsx)("rect", {
+  ry: "2",
+  rx: rx,
+  y: y,
+  x: x,
+  height: height,
+  width: width,
+  fill: color,
+  stroke: color
+});
+const IconAppLogo = _ref => {
+  let {
+    className,
+    title
+  } = _ref;
   return (0, _jsxRuntime.jsx)("span", {
     className: className,
     title: title,
-    children: (0, _jsxRuntime.jsxs)("svg", (0, _extends2["default"])({}, _SvgStyle.S_SVG, {
+    children: (0, _jsxRuntime.jsxs)("svg", {
+      ..._SvgStyle.S_SVG,
       viewBox: "0 0 32 32",
       xmlns: "http://www.w3.org/2000/svg",
       strokeMiterlimit: "2",
       strokeWidth: "2",
       children: [(0, _jsxRuntime.jsx)("title", {
         children: title
-      }), (0, _jsxRuntime.jsx)("rect", {
-        ry: "1.8825414",
-        rx: "194.14471",
-        y: "1.434558",
-        x: "19.176462",
-        height: "12.460618",
-        width: "10.85316",
-        fill: "#8ecc2d",
-        stroke: "#8ecc2d"
-      }), (0, _jsxRuntime.jsx)("rect", {
-        ry: "1.87537",
-        rx: "204.478",
-        y: "16.639841",
-        x: "8.8410215",
-        height: "13.752699",
-        width: "18.307165",
-        fill: "#232f3b",
-        stroke: "#232f3b"
-      }), (0, _jsxRuntime.jsx)("rect", {
-        ry: "1.87537",
-        rx: "204.478",
-        y: "3.4229634",
-        x: "2.5897937",
-        height: "10.894996",
-        width: "10.001963",
-        fill: "#a487d4",
-        stroke: "#a487d4"
-      })]
-    }))
+      }), _crRect(194, 1.5, 19, 12.5, 11, "#8ecc2d"), _crRect(204.5, 17.5, 9, 13.5, 18, "#232f3b"), _crRect(204.5, 3.5, 2.5, 11, 10, "#a487d4")]
+    })
   });
 };
-
-var _default = IconAppLogo;
-exports["default"] = _default;
+var _default = exports.default = IconAppLogo;
 //# sourceMappingURL=IconAppLogo.js.map
