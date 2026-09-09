fa4607fd705260c43924020fad5547c0289592f8
Yevhenii Molodyi <eugmolody@gmail.com>
2026-09-09 17:51:03 +0300

    Edit rspack.prod.js

--- rspack.prod.js
+++ rspack.prod.js
@@ -1,30 +1,30 @@
-'use strict'
+"use strict"
 
-const path = require('path')
-, HtmlWebpackPlugin = require('html-webpack-plugin')
-, babelConfig = require('./babel.config')
-, { rspack } = require('@rspack/core');
+const path = require("path")
+, HtmlWebpackPlugin = require("html-webpack-plugin")
+, babelConfig = require("./babel.config")
+, { rspack } = require("@rspack/core");
 
 
 module.exports = {
   mode: "production",
   cache: true,
   entry: {
-    app: {
-      import: path.resolve('src', 'index.jsx'),
-      dependOn: 'lib'
-    },
     lib: [
        "preact",
        "preact/compat",       
        "gl-matrix"
     ],
+    app: {
+      import: path.resolve("src", "index.jsx"),
+      dependOn: "lib"
+    }    
   },
   output: {
-      path: path.resolve('app'),
+      path: path.resolve("app"),
       filename: "[name]_[chunkhash].js",
       chunkFilename: "[name]_[chunkhash].js",
-      publicPath: 'app/'
+      publicPath: "app/"
   },
   module: {
     rules: [
@@ -32,7 +32,7 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
              cacheDirectory: true,
              ...babelConfig
@@ -46,8 +46,8 @@ module.exports = {
     ]
   },
   resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.jsx'],
+    modules: ["node_modules"],
+    extensions: [".js", ".jsx"],
     alias: { 
       "react": "preact/compat",      
       "react-dom": "preact/compat",
@@ -57,8 +57,8 @@ module.exports = {
   plugins : [    
     new HtmlWebpackPlugin({
       minify: false,
-      filename: path.resolve('index.html'),
-      template: path.resolve('template', 'index.ejs'),
+      filename: path.resolve("index.html"),
+      template: path.resolve("template", "index.ejs"),
       inject: false
     })
   ],
